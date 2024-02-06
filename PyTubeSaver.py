86bc2d8e9d9cd193d91f35549bfff365b1062274
Farida Ousmailou <faridaousmailou51@gmail.com>
2024-02-06 02:18:46 +0100

    code download

--- PyTubeSaver.py
+++ PyTubeSaver.py
@@ -1,5 +1,6 @@
-#   Projet "pytubesaver"
+# Projet "pytubesaver"
 # module pytube
+
 from pytube import YouTube
 
 url = "https://www.youtube.com/watch?v=XSy6ADgadoU"
@@ -8,16 +9,17 @@ def Download_progress(stream, chunk, bytes_remaining):
     pourcentage = bytes_downloaded * 100 / stream.filesize
     print(f"Progression du téléchargement: {int(pourcentage)}%")
 
+# Création de l'objet Youtube
 youtube_video = YouTube(url)
 
 print("Titre: " + youtube_video.title)
 print("NB VUES: ", youtube_video.views)
 
+# Listes des streams
 print("STREAMS")
 for stream in youtube_video.streams.fmt_streams:
     print(" ", stream)
 
-#stream = youtube_video.streams.get_by_itag(22)
 stream = youtube_video.streams.get_highest_resolution()
 print("Stream vidéo: " , stream)
 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pyTubeSaver
+Un programme permettant de télécharger n'importe quelle vidéo de la plate-forme YouTube.
