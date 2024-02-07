60b9393f3af32a57dede14b4c3f57b4e0d3e351d
Farida Ousmailou <faridaousmailou51@gmail.com>
2024-02-07 20:46:42 +0100

    code refactoring

--- PyTubeSaver.py
+++ PyTubeSaver.py
@@ -1,17 +1,13 @@
 # Projet "pytubesaver"
 
-#module pytube
+# module pytube
 from pytube import YouTube
 
 """Ce programme utilise le module pytube afin de créer deux fonctions permettant succèssivement de télécharger et
- l'extractions des métadpnnées sur n'importe quelle vidéo youtube"""
-
-def Download_video(url, emplacement):
+de l'extractions des métadpnnées sur n'importe quelle vidéo youtube"""
+def Download_video(youtube_video):
     try:
-        #Création d'objet YouTube avec l'URL de la vidéo
-        #youtube_video = YouTube(url)
-
-        # Afficher la liste des streams
+        # Affichage de la liste des streams
         print("STREAMS")
         for stream in youtube_video.streams.fmt_streams:
             print(" ", stream)
@@ -26,26 +22,38 @@ def Download_video(url, emplacement):
             print(f"Progression du téléchargement: {int(pourcentage)}%")
 
         #Téléchargement de la vidéo avec l'avénénement de la progression
-        print("Téléchragement en cours")
+        print("Téléchragement en cours...")
         youtube_video.register_on_progress_callback(Download_progress)
         stream.download()
         print("Fin de téléchargement")
 
     except:
-        print(f"Une erreur s'est produites")
+        print("Une erreur s'est produites")
 
-#Extractions des métadpnnées
+def Affichage_des_metadonnees (youtube_video):
+    try:
+        print("Titre de la vidéo: ", youtube_video.title)
+        print("Nombre de vues: ", youtube_video.views)
+        print("Auteur: ", youtube_video.author)
+        print("Description: ", youtube_video.description)
+    except:
+        print("Impossible d'afficher les métadonnées")
 
-"""print("Titre: " + youtube_video.title)
-    print("NB VUES: ", youtube_video.views)"""
+def main():
+    #Demande à l'utilisateur l'url et l'emplacement de la vidéo sur son repertoire
 
+    url_video = input("Entrez l'URL de la vidéo YouTube : ")
+    path = input ("Entrez le chemin du repertoire de destination : ")
 
-#Demande à l'utilisateur l'url et l'emplacement de la vidéo sur son repertoire
-url_vidéo = input("Entrez l'URL de la vidéo YouTube : ")
-Emplacement_vidéo = input ("Entrez le chemin du repertoire de destination : ")
+    #Création d'objet YouTube avec l'URL de la vidéo
+    youtube_video = YouTube(url_video)
 
-#Création d'objet YouTube avec l'URL de la vidéo
-youtube_video = YouTube(url_vidéo)
+    #Appel de la fonction Affichage des métandonnées
+    Affichage_des_metadonnees(youtube_video)
 
-#Appel de la fonction Download vidéo
-Download_video(url_vidéo, Emplacement_vidéo)
+    #Appel de la fonction Download vidéo
+    Download_video(youtube_video)
+
+if __name__ == "__main__":
+    main()
+
